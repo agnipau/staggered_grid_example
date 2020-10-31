266e3ed9ecdad3c76df81974198752824016c355
Matteo Guarda <matteoguarda@tutanota.com>
2020-10-31 14:54:27 +0100

    Update utils.dart

--- lib/utils.dart
+++ lib/utils.dart
@@ -1,10 +1,10 @@
 extension Max on List<num> {
   num maxOrNull() {
-    if (this.isEmpty) {
+    if (isEmpty) {
       return null;
     }
     var max = this[0];
-    for (var i = 1; i < this.length; ++i) {
+    for (var i = 1; i < length; ++i) {
       if (this[i] > max) {
         max = this[i];
       }
